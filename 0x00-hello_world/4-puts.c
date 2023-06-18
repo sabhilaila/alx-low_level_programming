805e056ad6d743f19b1e4bd3655527bfb479df94
sabhilaila <lailasabhi7@gmail.com>
2023-06-18 06:17:30 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+include 4-puts.c
+#include < studio.h >
+/**
+ * main - Entry point 
+ * return: Always 0 (Success)
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");return (0) ;
+}
