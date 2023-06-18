8a0b08fda4abd31061d9f710fd94c6a4dd8ec677
sabhilaila <lailasabhi7@gmail.com>
2023-06-18 07:00:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,5 +6,6 @@ include 4-puts.c
  */
 int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle");return (0) ;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
