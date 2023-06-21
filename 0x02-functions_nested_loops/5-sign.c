668b90b9b944923cf5cd1befb01fe5a6e73705a6
sabhilaila <lailasabhi7@gmail.com>
2023-06-21 12:11:48 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,22 +9,22 @@
  * -1 is less than zero
  */
 
-int print_sign (int n)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(43);
-		return (1);
+	_putchar(43);
+	return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar(45);
-                return (-1);
+	_putchar(45);
+	return (-1);
 	}
 	else 
 	{
-		_putchar(48);
-                return (0);
+	_putchar(48);
+	return (0);
 	}
 	_putchar('\n');
 }
