32beb13fe7eafac23c19af7bb8b40311354cceaa
sabhilaila <lailasabhi7@gmail.com>
2023-06-26 00:57:08 -0700

    0-positive_or_negative.c

---
+++ 0-isupper.c
@@ -0,0 +1,10 @@
+#ifndef main.h
+#define main.h
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int &, int b);
+void print_numbers(void);
+void print more_numbers(void);
+void print more_more_numbe
+#endif
