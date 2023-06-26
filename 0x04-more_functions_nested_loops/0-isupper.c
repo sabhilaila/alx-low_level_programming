acdeb432ba0f56956c6b6ad60f95015dbd9ff4ba
sabhilaila <lailasabhi7@gmail.com>
2023-06-26 15:44:54 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_isupper - check if a letter is upper 
+ *_isupper - check if a letter is upper
  *@x: the number to be checked
  *Return: 1 for upper letter or 0 for any else
  */
