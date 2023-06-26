9bade5687fe1bbf8a0a72242e8c3f10a53380ec9
sabhilaila <lailasabhi7@gmail.com>
2023-06-26 09:29:32 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,14 +4,14 @@
  * -puts - prints a string
  *  @str: the string to print
  *  Return: void
- *  */
+ */
 
 void _puts(char *str)
 
 {
 	for (; *str != '\0'; str++)
 {
-	_putchar(*str); 
+	_putchar(*str);
 }
 	_putchar('\n');
 }
