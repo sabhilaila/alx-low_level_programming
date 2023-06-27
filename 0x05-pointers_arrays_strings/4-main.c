a17287dd13646d74461a5f0f93a299d30e312bf1
sabhilaila <lailasabhi7@gmail.com>
2023-06-27 16:16:22 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-main.c
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -2,12 +2,14 @@
 
 /**
  * main - check the code
- *
+ *@str: the string to be printed in reverse
  * Return: Always 0.
  */
+
 void print_rev(char *str)
 
 {
+
     char *str;
 
     str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
