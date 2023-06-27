8260d97b690a4905e24be04775220c206611ec05
sabhilaila <lailasabhi7@gmail.com>
2023-06-27 16:56:05 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
-
 * puts_half - Prints half of a string
 * @str: The string to print
 * Return: void
@@ -21,7 +20,7 @@ j++;
 if (j % 2 == 1)
 {
 k = (j - 1) / 2;
-k += 1; 
+k += 1;
 }
 
 else
