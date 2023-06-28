9998d0acdc56104280b0fae5dd41457af13d218f
sabhilaila <lailasabhi7@gmail.com>
2023-06-28 01:21:43 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ * main - prints a text accoriding number
+ * Return: always (Success)
+ *
+ */
+
+int main(void)
+
+{
+	char low;
+
+	for ('low' = 'z'; low >= 'a'; low--)
+	{
+		putchar(low);
+	}
+		putchar('\n');
+
+	return (0);
+}
