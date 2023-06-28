bf7473c4837006419d98abe42bc6c5e7e45fc78b
sabhilaila <lailasabhi7@gmail.com>
2023-06-28 04:18:58 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,11 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - prints all natural numbers from n to 98
  * @n: the number to start pinting from
- * Return: Always 0.
+ * Return: void
  */
-void print_to_98(int n);
+void print_to_98(int n)
 
 {
 	if (n <= 98)
