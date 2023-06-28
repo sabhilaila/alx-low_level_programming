f4c7c0cd3d9d1b36781d5a063d68df140ff68dd0
sabhilaila <lailasabhi7@gmail.com>
2023-06-28 04:30:34 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,8 +15,7 @@ void print_to_98(int n)
 	{
 		if (n == 98)
 	{
-		printf("%d", n);
-		printf('\n');
+		printf("%d\n", n);
 		break;
 	}
 		else
@@ -31,8 +30,7 @@ void print_to_98(int n)
 	{
 		if (n == 98)
 	{
-		printf("%d", n);
-		printf('\n');
+		printf("%d\n", n);
 		break;
 	}
 		else
@@ -41,4 +39,4 @@ void print_to_98(int n)
 	}
 	}
 	}
-
+}	
