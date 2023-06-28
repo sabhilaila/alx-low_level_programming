ebb679008ec6fc0c594e4128d0f1ac01f73ec350
sabhilaila <lailasabhi7@gmail.com>
2023-06-28 01:32:02 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ * main - prints a text accoriding number
+ * Return: always (Success)
+ *
+ */
+
+int main(void)
+
+{
+	int d;
+	for (d = '0'; d <= '9'; d++)
+	{
+	putchar(d);
+	if (d != '9')
+	{
+	putchar(',');
+	ptchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
