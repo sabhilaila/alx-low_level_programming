b74da56f6e0e48f7d0fea8952309a1f31cfcb391
sabhilaila <lailasabhi7@gmail.com>
2023-06-28 23:54:20 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,14 +12,14 @@ void print_line(int n)
 
 	if (n <= 0)
 	{
-	_putchar('\n');
+	putchar('\n');
 	}
 	else
 	{
 	for (x = 0; x < n; x++)
 	{
-	_putchar(95);
+	putchar(95);
 	}
-	_putchar('\n');
+	putchar('\n');
 	}
 }
