186cac8ee6979a6f1d75da0523e1939cba4e1a15
sabhilaila <lailasabhi7@gmail.com>
2023-06-30 03:06:20 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,13 +7,16 @@
  *
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
 	int index = 0;
-	while (str[index]);
+
+	while (str[index] != '\0')
 	{
 	if (str[index] >= 'a' && str[index] <= 'z')
+	{
 		str[index] -= 32;
+	}
 		index++;
 	}
 	return (str);
