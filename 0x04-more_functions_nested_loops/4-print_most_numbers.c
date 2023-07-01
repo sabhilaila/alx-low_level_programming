1b2bf4cb326207eaeb1bb1241f665af592eecb17
sabhilaila <lailasabhi7@gmail.com>
2023-07-01 15:46:44 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,18 +11,18 @@ void print_most_numbers(void)
 
 {
 
-int x = 0;
+	int x = 0;
 
-for (; x <= 9; x++)
-{
-if (x == 2 || x == 4)
-{
-continue;
-}
-else
-{
-putchar(x);
-}
-}
-putchar('\n');
+	for (; x <= 9; x++)
+	{
+	if (x == 2 || x == 4)
+	{
+	continue;
+	}
+	else
+	{
+	putchar(x + '0' );
+	}
+	}
+	putchar('\n');
 }
