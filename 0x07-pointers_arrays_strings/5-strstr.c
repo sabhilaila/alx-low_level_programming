bee1c20dc880c611ce91c5937d6003efeab15479
sabhilaila <lailasabhi7@gmail.com>
2023-07-04 15:09:12 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
- * _strstr: entry point
+ * _strstr - entry point
  * @haystack: input
  * @needle: input
  *
- * Return: Always 0 (Success)
+ * Return: needle or null
  */
 
 char *_strstr(char *haystack, char *needle)
