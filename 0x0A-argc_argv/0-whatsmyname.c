b3b12046cd0421bf9f41263db9f11709eb1cd200
sabhilaila <lailasabhi7@gmail.com>
2023-07-23 04:29:18 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,12 +5,13 @@
  * main - entry point
  * @argc: input
  * @argv: input
- * Return. always 0
+ * Return: always 0
  */
 
 int main(int argc, char *argv[])
 {
-	(void) argc;
-       printf("%s\n", argv[0]);
-return (0);       
+		(void) argc;
+
+		printf("%s\n", argv[0]);
+	return (0);
 }
