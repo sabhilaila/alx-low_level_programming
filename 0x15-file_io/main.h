43c2340b1652e87822dd05940f3663cf92018fbb
sabhilaila <lailasabhi7@gmail.com>
2023-08-07 17:36:44 -0700

    0-read_textfile.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stddef.h>
 
-int main(int ac, char **av);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
