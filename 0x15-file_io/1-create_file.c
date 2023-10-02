f6c3a704894af79a1209e0063774ebebb9d0855b
sabhilaila <lailasabhi7@gmail.com>
2023-10-02 10:39:14 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include "main.h"
+#include <string.h>
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+
+if (filename == NULL)
+{
+return (-1);
+}
+file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+if (file == -1)
+{
+return (-1);
+}
+if (text_content != NULL)
+{
+ssize_t written = write(file, text_content, strlen(text_content));
+if (written == -1)
+{
+close(file);
+return (-1);
+}
+}
+close(file);
+return (1);
+}
+
