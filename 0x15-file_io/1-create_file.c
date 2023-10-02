e5439ab7e6b9c61b08a4d4f279e7dd1fb8b644d9
sabhilaila <lailasabhi7@gmail.com>
2023-10-02 12:00:37 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,8 @@
 #include <string.h>
 /**
  * create_file - entry point
- *
+ * @filename: input
+ * @text_content: input
  * Return: 0 for success
  */
 int create_file(const char *filename, char *text_content)
